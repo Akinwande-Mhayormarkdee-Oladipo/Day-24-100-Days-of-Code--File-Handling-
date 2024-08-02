a711d99419bef80f0a8713d4e15ee325ad92e460
Akinwande Oladipo <54485470+Akinwande-Mhayormarkdee-Oladipo@users.noreply.github.com>
2024-08-02 16:18:04 +0100

    update on main.py

--- main.py
+++ main.py
@@ -2,5 +2,21 @@
 #     contents = file.read()
 #     print(contents)
 
-with open("my_file.txt", mode='a') as file:
-    file.write("\nNew New New")
+# with open("my_file.txt", mode='a') as file:
+#     file.write("\nNew New New")
+
+# with open("weather_data.csv") as weather_file:
+#     weather = weather_file.readlines()
+#     print(weather)
+
+# import csv
+#
+# with open('weather_data.csv') as weather:
+#     weather = csv.reader(weather)
+#     temp = []
+#     for row in weather:
+#         if row[1] != 'temp':
+#             temp.append(int(row[1]))
+#     print(temp)
+
+import pandas
